2ce12309dea58110c9793f854c8e49f43333da2e
Kifrx <zik.min02@gmail.com>
2025-12-06 18:39:15 +0700

    Update UserSession.java

--- src/UserSession.java
+++ src/UserSession.java
@@ -1,22 +1,17 @@
 public class UserSession {
-   
+    
     private static String username = "admin";
     private static String password = "123";
 
-    //Cek Login
-    public static boolean cekLogin(String user, String pass) {
-        if (user.equals(username) && pass.equals(password)) {
-            return true;
-        } else {
-            return false;
-        }
-    }
 
-    public static void registrasiBaru(String userBaru, String passBaru) {
-        username = userBaru;
-        password = passBaru;
+    public static boolean cekLogin(String user, String pass) {
+        return user.equals(username) && pass.equals(password);
     }
-    
+   
+    public static void daftarBaru(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
     public static String getUsername() {
         return username;
     }
